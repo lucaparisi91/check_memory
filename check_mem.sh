4a6ba6cea6417090370994535f83418497635ce5
lucaparisi91 <luca.parisi91@gmail.com>
2023-05-08 14:04:55 +0100

    scripts takes nJobs and inverval options

--- check_mem.sh
+++ check_mem.sh
@@ -1,16 +1,39 @@
+#!/bin/bash
 
-interval=10
-nJobs=10
+interval=1 # N of seconds between memory queries
+nJobs=10 # N top jobs per memory usage to track
+base_filename=mem
+
+
+while getopts n:t: flag
+do
+    case "${flag}" in
+        t) interval=${OPTARG};;
+        n) nJobs=${OPTARG};;
+    esac
+done
+
+
+
 i=0
-filename=mem.dat
+
+echo "Step MemFraction VirtMem ResMem pid" > "$base_filename.dat" 
+
+echo "Step Memory" > "${base_filename}_sys.dat"
+
 
 while [ 1 ]
-echo "Step MemFraction VirtMem ResMem pid" > $filename
-do
-    ps aux | sort -k 4 -r > tmp.dat  
-    head -n $((nJobs + 1)) tmp.dat | tail -n $nJobs | awk "{print $i,\$4,\$5,\$6,\$2 }" >> $filename
-    sleep $interval
-    rm tmp.dat
-    i=$((i+interval))
 
+
+do
+    # saves memory usage per process from ps
+    ps aux | sort -k 4 -r > .tmp.dat  
+    head -n $((nJobs + 1)) .tmp.dat | tail -n $nJobs | awk "{print $i,\$4,\$5,\$6,\$2 }" >> "${base_filename}.dat"
+    
+    # saves system wide memory usage
+    free --kilo | grep Mem | awk "{print $i,\$3}" >> "${base_filename}_sys.dat"
+    sleep $interval
+
+    i=$((i+interval))
+    
 done
