aece29e89fed6b8a8cf77249bf6ab8d0b0f843ec
lucaparisi91 <luca.parisi91@gmail.com>
2023-05-11 18:23:11 +0100

    More robust way to extract memory per process. Filtering by sorting is disabled.

--- check_mem.sh
+++ check_mem.sh
@@ -6,19 +6,17 @@ interval=1 # N of seconds between memory queries
 nJobs=10 # N top jobs per memory usage to track
 base_filename=mem
 
-usage=$"$(basename "$0") [-h] [-t n] [-n n] \n
+usage=$"$(basename "$0") [-h] [-t n] \n
 
 where: \n
     -h show this help text \n
-    -t sampling interval in seconds \n
-    -n number of top jobs per memory consumption to save \n
+    -t sampling interval in seconds. Must be an integer \n
 "
 
-while getopts n:t:h flag
+while getopts t:h flag
 do
     case "${flag}" in
         t)  interval=${OPTARG};;
-        n)  nJobs=${OPTARG};;
         h)  echo -e $usage 1>&2
             exit 1
             ;;
@@ -30,19 +28,18 @@ done
 
 i=0
 
-echo "Step MemFraction VirtMem ResMem pid" > "$base_filename.dat" 
+echo "Step Memory Pid Name" > "$base_filename.dat" 
 
 echo "Step Memory" > "${base_filename}_sys.dat"
 
 
 while [ 1 ]
 
-
 do
     # saves memory usage per process from ps
-    ps aux | sort -k 4 -r > .tmp.dat  
-    head -n $((nJobs + 1)) .tmp.dat | tail -n $nJobs | awk "{print $i,\$4,\$5,\$6,\$2 }" >> "${base_filename}.dat"
-    
+
+    ps -F | tail -n +2 | awk "{print $i,\$6,\$2,\$11 }"  >> "${base_filename}.dat"
+
     # saves system wide memory usage
     free --kilo | grep Mem | awk "{print $i,\$3}" >> "${base_filename}_sys.dat"
     sleep $interval
